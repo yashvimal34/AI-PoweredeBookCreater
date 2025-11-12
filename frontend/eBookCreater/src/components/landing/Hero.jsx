c0140b123988d577c7a7c10043b871954ed5f127
Yash Vimal <yashsarthak79@gmail.com>
2025-11-12 22:26:27 +0530

    image added

--- frontend/eBookCreater/src/components/landing/Hero.jsx
+++ frontend/eBookCreater/src/components/landing/Hero.jsx
@@ -1,3 +1,8 @@
+import { ArrowRight, Sparkles, BookOpen, Zap } from "lucide-react";
+import { usrAuth } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+import Hero_IMG from "../../assets/hero.jpg";
+
 const Hero = () => {
     return (
         <div>Hero</div>
