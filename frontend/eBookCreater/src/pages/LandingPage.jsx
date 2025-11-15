1335debe70e1d33e00e3a0afefcd64095cbced2d
Yash Vimal <yashsarthak79@gmail.com>
2025-11-15 21:58:17 +0530

    half implement

--- frontend/eBookCreater/src/pages/LandingPage.jsx
+++ frontend/eBookCreater/src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
+import Features from "../components/landing/Features"
 import Hero from "../components/landing/Hero"
 import Navbar from "../components/layout/Navbar"
 
 const LandingPage = () => {
     return (
-        <div>
+        <div className="mb-[100vh]">
             <Navbar />
             <Hero />
+            <Features />
         </div>
     )
 }
